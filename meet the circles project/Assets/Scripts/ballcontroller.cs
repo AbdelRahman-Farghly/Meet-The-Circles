ade22cdb01867c068996c41f74f2ddc4bb4f30a8
Micheline Medhat <MichelineMedhat@gmail.com>
2019-07-16 03:16:34 +0200

    Add Replay Mode

--- meet the circles project/Assets/Scripts/ballcontroller.cs
+++ meet the circles project/Assets/Scripts/ballcontroller.cs
@@ -30,7 +30,7 @@ public class ballcontroller : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag=="limits")
         {
-            Debug.Log("you lose");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             Destroy(this.gameObject);
         }
     }
